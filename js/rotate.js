cb84be756b663f478e7e2960b5e09a50afc2ab64
DInnaD <DInnaD@users.noreply.github.com>
2020-03-02 11:54:33 +0200

    Create rotate.js

---
+++ js/rotate.js
@@ -0,0 +1,10 @@
+(function() {
+function changeClass(){
+    if(document.getElementById("block").className == "block"){
+        document.getElementById("block").className += " rotated";
+    }
+    else{
+        document.getElementById("block").className = "block";
+    }
+}
+})();
